84d8df28bcf102ae12fd6b832197eed012e525c5
basketcase368 <basketcase368@gmail.com>
2016-07-09 10:40:30 -0700

    Add project files.

---
+++ NinjaDomain.Classes/Enums/NinjaType.cs
@@ -0,0 +1,9 @@
+﻿namespace NinjaDomain.Classes.Enums
+{
+    // NOTE: By defualt types are all private. Must be decalred public intentiaonally
+    public enum NinjaType
+    {
+        Shinobi = 1,
+        Kunoichi = 2
+    }
+}
