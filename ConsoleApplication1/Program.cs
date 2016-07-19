cfef466499dbf638057a9d5e360aab85749009b8
basketcase368 <basketcase368@gmail.com>
2016-07-18 22:20:30 -0700

    Added method insertMultipleNinjas to add mutliple ninjas at a time. Changed casing of private methods to match conventions

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.Entity;
+using System.Collections.Generic;
 using NinjaDomain.Classes;
 using NinjaDomain.DataModel;
 
@@ -13,11 +14,12 @@ namespace ConsoleApplication1
             Database.SetInitializer(new NullDatabaseInitializer<NinjaContext>());
 
             // Methods demonstrating EF functionality
-            InsertNinja();
+            insertNinja();
+            insertMultipleNinjas();
             Console.ReadKey();
         }
 
-        private static void InsertNinja()
+        private static void insertNinja()
         {
             var ninja = new Ninja
             {
@@ -27,10 +29,37 @@ namespace ConsoleApplication1
                 ClanId = 1
             };
 
-            using(var context = new NinjaContext())
+            using (var context = new NinjaContext())
             {
                 context.Database.Log = Console.WriteLine;
                 context.Ninjas.Add(ninja);
+                context.SaveChanges();
+            }
+        }
+
+        private static void insertMultipleNinjas()
+        {
+            var ninja1 = new Ninja
+            {
+                Name = "Leonardo",
+                ServedInOniwaban = false,
+                DateOfBirth = new DateTime(1984, 1, 1),
+                ClanId = 1
+            };
+
+            var ninja2 = new Ninja
+            {
+                Name = "Raphael",
+                ServedInOniwaban = false,
+                DateOfBirth = new DateTime(1985, 1, 1),
+                ClanId = 1
+            };
+
+            // using takes care of closing this connection on failure
+            using (var context = new NinjaContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                context.Ninjas.AddRange(new List<Ninja> { ninja1, ninja2 });
                 context.SaveChanges();
             }
         }
